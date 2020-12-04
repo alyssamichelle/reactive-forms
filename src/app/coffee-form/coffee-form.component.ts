28fa0350388563582e1d905e9afc25543b4e31bd
Alyssa Nicoll <alyssaamnell@gmail.com>
2020-12-04 16:39:59 -0600

    start setting up intial form

--- src/app/coffee-form/coffee-form.component.ts
+++ src/app/coffee-form/coffee-form.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'coffee-form',
   templateUrl: './coffee-form.component.html',
   styleUrls: ['./coffee-form.component.scss']
 })
-export class CoffeeFormComponent implements OnInit {
+export class CoffeeFormComponent {
+  coffeeOrderForm: FormGroup;
+  specialRequestsControls: FormArray;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder ) { 
+    this.buildForm();
   }
 
+  buildForm(){
+    this.coffeeOrderForm = this.formBuilder.group({
+      customerName: this.formBuilder.control(null),
+      specialRequests: this.formBuilder.array([
+        this.formBuilder.control(null)
+      ])
+    });
+
+    this.specialRequestsControls = this.coffeeOrderForm.get('specialRequests') as FormArray;
+  }
+
+  onAddSpecialRequest() {
+    this.specialRequestsControls.push(this.formBuilder.control(null));
+  }
+
+  onRemoveSpecialRequest(index) {
+    this.specialRequestsControls.removeAt(index);
+  }
+
+  onResetForm() {
+    this.coffeeOrderForm.reset();
+  }
+
+  onSubmitForm() {
+    // submit form to service
+
+    console.log(this.coffeeOrderForm.value);
+  }
 }
